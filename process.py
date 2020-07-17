8b6c0ea37020cdc5ca06eecb2f1f2a8caf553bad
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-16 19:07:51 -0700

    Added comment & changed to Mon/ Final Commit

--- process.py
+++ process.py
@@ -1,12 +1,13 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") #opening the file 
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): #function taking file as parameter
+    """Print sales info for Monday"""
+    for line in log_file: #looping through each line of the file
+        line = line.rstrip() #removing characters in each line
+        day = line[0:3] #identifying the first three characters as day
+        if day == "Mon": #if day is Monday 
+            print(line) #print that line 
 
 
-sales_reports(log_file)
+sales_reports(log_file) #calling the function with file as arg
